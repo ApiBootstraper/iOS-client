4cb05179a7355f35f617e04035237e11cce3c46f
Jérémy Chaufourier <jeremy@opsone.net>
2013-02-27 14:04:37 +0100

    Init app v1.0

---
+++ v1.0/ApiBootstraper/ApiBootstraper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ApiBootstraper
+//
+//  Created by Jeremy on 27/02/13.
+//  Copyright (c) 2013 apibootstraper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ v1.0/ApiBootstraper/ApiBootstraper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ApiBootstraper
+//
+//  Created by Jeremy on 27/02/13.
+//  Copyright (c) 2013 apibootstraper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
